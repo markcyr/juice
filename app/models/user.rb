b12b161820f2fe2d262e541264cd46c2a8f0dc31
Azbagheri <azadeh.bagheri7@gmail.com>
2017-05-05 17:40:52 -0400

    changing the associations in the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   has_many :reviews
-  has_many :recipes, through: :reviews
+  has_many :reviewed_recipes, class_name: "Recipe", through: :reviews
 
-  has_many :owned_recipes, class_name: "Recipe", foreign_key: "user_id"
+  has_many :recipes
 
 
 end
