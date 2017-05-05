72f170f2cc13f2a7f41b6270ad64e8334da50bd6
Azbagheri <azadeh.bagheri7@gmail.com>
2017-05-05 17:29:58 -0400

    Adding associations fo the Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ApplicationRecord
   has_many :ingredients_recipes
   has_many :ingredients, through: :ingredients_recipes
+
+  has_many :reviews
+  has_many :users, through: :reviews
+
+  belongs_to :user
 end
