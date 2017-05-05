61bbd3e17ef873339ece8f9da28f4a4712b045d7
Azbagheri <azadeh.bagheri7@gmail.com>
2017-05-05 17:33:51 -0400

    Adding associations fo the Recipe User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  has_many :reviews
+  has_many :recipes, through: :reviews
+
+  has_many :owned_recipes, class_name: "Recipe", foreign_key: "user_id"
+
+
 end
