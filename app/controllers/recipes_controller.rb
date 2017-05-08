3c1dabb99faafb7d914705d57d706f2923227387
Swapna Brungi <swapna.brungi@gmail.com>
2017-05-08 16:27:07 -0400

    added recipes controller methods and recipes views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,22 @@
 class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    # @user_name = @user_name.find(params_id)
+   @recipe.order(:created_at).reverse
+  end
+
+  def create
+    @recipe = Recipe.new()
+    @recipe.user = current_user
+  end
+private
+  def recipe_params
+    params.require(:recipe).[permit :name, :image, :user_id]
+
+  end
 end
