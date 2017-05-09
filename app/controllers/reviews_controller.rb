45e00bee7dd844a362c81b9c4ca45efde0b5bcfb
Swapna Brungi <swapna.brungi@gmail.com>
2017-05-09 16:30:57 -0400

    created review section on recipes show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,32 @@
 class ReviewsController < ApplicationController
 
-def show
-  @review = Review.find(params[:id])
-end
+  def show
+    @review = Review.find(params[:id])
+  end
 
+  def created
+    @recipe = Recipe.find(params[:recipe.id])
+    @review = @recipe.reviews.build(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to recipes_path,
+      notice: 'Review created succesfully'
+    else
+      render 'recipes/show
+    end'
+  end
+
+
+
+
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :recipe_id)
+  end
+  def
+    @recipe.find(params[:recipe_id])
+  end
 end
