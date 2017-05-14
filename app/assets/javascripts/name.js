1ac6fc32463b55b9d9572b0633b57df5531e4abf
Azbagheri <azadeh.bagheri7@gmail.com>
2017-05-13 22:07:33 -0400

    Fixed name.js

--- app/assets/javascripts/name.js
+++ app/assets/javascripts/name.js
@@ -6,15 +6,11 @@ function randomName () {
 var adjective = ["Abundant", "Adorable", "Adventurous","Alluring", "Amazing", "Ambitious", "Amusing" ,"Aromatic", "Astonishing", "Attractive", "Awesome", "Beautiful", "Bizarre", "Blushing", "Brainy", "Brave", "Breezy", "Bright", "Bustling", "Classy", "Classic", "Crafted","Delectable","Delightful", "Extraordinary", "Flavourful", "Generous", "Gorgeous", "Heavenly", "Intense", "Juicy","Intriguing", "Lavish", "Lean", "Lip-smacking", "Lively", "Luscious", "Perfect", "Popular", "Rich", "Skillful", "Smooth", "Soothing", "Special", "Superb", "Tasteful", "Tasty", "Terrific", "Chilled", "Cool", "Delicious", "Fresh", "Fantastic", "Famous", "Happy", "Nutritious" ];
 
 
-var characters = ["Monster", "Diva", "King", "Princess", "Prince", "Queen", "Giant", "Fighter", "Wizard", "Hero", "Heroine", "Explorer", "Rebel", "Ruler", "Boss", "God", "Goddess", "Challenger", "Warrior",0 "Badboy", "Contender", Cat-lady", "Bad-girl", "Drama-Queen", "Southern Bell", "Soulmate", "Ninja", "Knight", "Droid", "Hacker", "Robot" ];
+var characters = ["Monster", "Diva", "King", "Princess", "Prince", "Queen", "Giant", "Fighter", "Wizard", "Hero", "Heroine", "Explorer", "Rebel", "Ruler", "Boss", "God", "Goddess", "Challenger", "Warrior", "Badboy", "Contender", "Cat-lady", "Bad-girl", "Drama-Queen", "Southern Bell", "Soulmate", "Ninja", "Knight", "Droid", "Hacker", "Robot" ];
 
 // var randonNumber1 = parseInt(Math.random() * first.lenght);
 // var randonNumber2 = parseInt(Math.random() * lanst.lenght);
 // var adjIngredchar = adjective[randomNumber] + " " + characters[randomNumber2];
-
-if (
-}
-}
 
 
 };
