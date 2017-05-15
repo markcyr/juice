6c12b646fbcdcec35abf0c74364edad4ae468266
Azbagheri <azadeh.bagheri7@gmail.com>
2017-05-14 21:52:00 -0400

    Adding the method daily_value to the recipe model to calcuate the daily value percentage of nutrients in a recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,8 +8,8 @@ class Recipe < ApplicationRecord
   belongs_to :user
   accepts_nested_attributes_for :ingredients_recipes
 
-
-  def get_nutrient(nutr)
+# get_nutrient method calculates the amount of a nutrient per serving size
+  def get_nutrient(nutrient)
     nutrients = {protein: 'protein_g', iron: 'iron_mg', calcium: 'calcium_mg', calory: 'energy_kcal', fat: 'lipid_tot_g',
        vitC: 'vit_c_mg', VitA: 'vit_a_rae', sodium: 'sodium_mg', sugar: 'sugar_tot_g', carbohydrate: 'carbohydrt_g', fiber: 'fiber_td_g'  }
       #  serving size for juice is 125 mL
@@ -32,14 +32,23 @@ class Recipe < ApplicationRecord
           g = 454
       end
       size += (q*g)
-      v = ingredient_recipe.ingredient.send("#{nutrients[nutr.to_sym]}")
+      # v is the amount of nutrient per 100 gram of an ingredient
+      v = ingredient_recipe.ingredient.send("#{nutrients[nutrient.to_sym]}")
       ingredient_nutrient << (q*g)*(v/100.0)
     end
-    
-      total_per_serving =  (ingredient_nutrient.sum)/(size/125.0)
+    total_per_serving =  (ingredient_nutrient.sum)/(size/125.0)
+
     return total_per_serving.round(2)
   end
 
+  def daily_value(nutrient)
+    # recommended daily intake of nutrients is stored in rdi
+    rdi = {iron: 14, calcium: 1100, fat: 65,vitC: 60, VitA: 1000, sodium: 2400, carbohydrate: 300, fiber: 25 }
+    serving_amount = self.get_nutrient(nutrient)
+    nutrient_rdi = rdi[nutrient.to_sym]
+    davily_value = (serving_amount / nutrient_rdi) * 100
+    return davily_value.round(2)
+  end
 
 
 end
