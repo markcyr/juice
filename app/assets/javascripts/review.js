d16240ce7274e6eb74ed935b481f70d4705a85d7
markcyr <markcyr@msn.com>
2017-05-15 18:40:45 -0400

    added ajax to recipe show page

---
+++ app/assets/javascripts/review.js
@@ -0,0 +1,32 @@
+$(function() {
+
+  // Listen for the submit form
+
+  $('#new_review').on('submit', function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      method:$(this).attr('method'),
+      url: $(this).attr('action'),
+      data: $(this).serialize(),
+      dataType: 'html'
+
+    }).done(function(data){
+      $("#reviewoutput").prepend(data);
+    $('#review_comment').val('');
+
+      console.log('submit was successful');
+      console.log(data);
+
+    }).fail(function(){
+        console.log('submit failed');
+
+    }).always(function(){
+
+    })
+
+  });
+
+
+
+});
