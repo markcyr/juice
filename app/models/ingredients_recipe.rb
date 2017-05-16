fe3305927452d6850f7716f0e08736002bcb1a1a
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-05-16 17:19:00 -0400

    quantity validation msg

--- app/models/ingredients_recipe.rb
+++ app/models/ingredients_recipe.rb
@@ -4,5 +4,12 @@ class IngredientsRecipe < ApplicationRecord
 
   belongs_to :unit
 
-    validates :quantity, numericality: { greater_than: 0}
+  # validates :quantity, numericality: { greater_than: 0}
+#   # validates :quantity, numericality: { message: "%{value} needs to be a number" }
+  validate :quantity_must_be_a_number
+
+  def quantity_must_be_a_number
+      errors.add(:base, "Quantity MUST be positive")
+  end
+
 end
