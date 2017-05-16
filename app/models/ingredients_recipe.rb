95cc522f244d11d8c703d00a2780e3f9925a9fb8
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-05-16 17:32:14 -0400

    fixed error msg

--- app/models/ingredients_recipe.rb
+++ app/models/ingredients_recipe.rb
@@ -4,12 +4,13 @@ class IngredientsRecipe < ApplicationRecord
 
   belongs_to :unit
 
-  # validates :quantity, numericality: { greater_than: 0}
+  validates :quantity, numericality: { greater_than: 0}
 #   # validates :quantity, numericality: { message: "%{value} needs to be a number" }
-  validate :quantity_must_be_a_number
+  # validate :quantity_must_be_a_number
+  #
+  # def quantity_must_be_a_number
+  #     errors.add(:base, "Please make QTY a positive number")
+  #   end
 
-  def quantity_must_be_a_number
-      errors.add(:base, "Quantity MUST be positive")
-  end
 
 end
