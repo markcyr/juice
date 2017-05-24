70f1022ff4bf75df951a9adb0617ca48dfa55fe5
Azbagheri <azadeh.bagheri7@gmail.com>
2017-05-24 19:44:59 -0400

    The flash message for comment errors is disapearing after the error is corrected

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -8,11 +8,12 @@ $(function() {
       data: $(this).serialize(),
       dataType: 'json'
     }).done(function(data) {
-
+      $('comment_box span').val('');
       if(data.errors.length !== 0){
         var error = $('<span>').appendTo('.comment_box')
         $(error).html('<p>' + data.errors[0]+'</p>')
       }else{
+        $('.comment_box span').html(' ');
       var review = data['review'],
         comment = $('<li>').prependTo('.reviews'),
         commenter = $('<li>').prependTo('.reviews'),
@@ -30,11 +31,12 @@ $(function() {
           $(' ').appendTo(rate);
         }
         if(data.review_count === 1 ){
-          $('.review_link').html(data.review_count.toString() + 'review')
+          $('.review_link').html(data.review_count.toString() + 'review');
         }else{
-          $('.review_link').html(data.review_count.toString() + 'reviews').css('color', '#337ab7')
+          $('.review_link').html(data.review_count.toString() + 'reviews').css('color', '#337ab7');
         }
         $('#review_comment').val('');
+
         // $('.modal').fadeIn();
         // $('button.close').on('click', function(){
         //     $('.modal').fadeOut('slow');
@@ -48,6 +50,7 @@ $(function() {
 
     }).always(function() {
        $('input[type="submit"]').removeAttr('disabled');
+
     }).error(function()   {
 
     });
