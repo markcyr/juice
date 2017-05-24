5d6434aefa03208aa865cc8ae1796f60beb9a6d1
Azbagheri <azadeh.bagheri7@gmail.com>
2017-05-23 23:07:11 -0400

    Setting up carrierwave to upload pictures to googlecloud

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,12 +1,12 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-   #include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::RMagick
+   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  #storage :file
+   storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -15,6 +15,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
+
    def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
     ActionController::Base.helpers.asset_path("recipeimages/#{rand(6)+1}.jpg")
@@ -24,7 +25,16 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
+  # def default_url(*args)
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  # Process files as they are uploaded:
+
+  #process resize_to_fit: [200, 300]  #
   # def scale(width, height)
   #   # do something
   # end
@@ -40,6 +50,11 @@ class ImageUploader < CarrierWave::Uploader::Base
      %w(jpg jpeg gif png)
    end
 
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  # def extension_whitelist
+  #   %w(jpg jpeg gif png)
+  # end
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
