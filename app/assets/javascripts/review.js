d5ab1fbb66d13942758f3d3c7304d288d0b21563
Azbagheri <azadeh.bagheri7@gmail.com>
2017-05-24 19:24:38 -0400

    Number of reviews is updated on submit clicks

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -8,7 +8,7 @@ $(function() {
       data: $(this).serialize(),
       dataType: 'json'
     }).done(function(data) {
-      
+
       if(data.errors.length !== 0){
         var error = $('<span>').appendTo('.comment_box')
         $(error).html('<p>' + data.errors[0]+'</p>')
@@ -28,7 +28,11 @@ $(function() {
           $(star).addClass('single_star');
           $(star).appendTo(rate);
           $(' ').appendTo(rate);
-
+        }
+        if(data.review_count === 1 ){
+          $('.review_link').html(data.review_count.toString() + 'review')
+        }else{
+          $('.review_link').html(data.review_count.toString() + 'reviews').css('color', '#337ab7')
         }
         $('#review_comment').val('');
         // $('.modal').fadeIn();
