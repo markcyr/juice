bdebb777b936c84921ecf6f368bc5b826c9b174d
markcyr <markcyr@msn.com>
2017-05-24 22:19:19 -0400

    updated home articles and users pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
    @user = User.find(params[:id])
-     @recipes = @user.recipes.paginate(:page => params[:page], :per_page =>2)
+     @recipes = @user.recipes.paginate(:page => params[:page], :per_page =>3)
 
   end
 
