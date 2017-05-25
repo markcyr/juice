a9f78b85c660050f92ef19457857f2cedbd90a31
PAUL LAM <PAULLAMX@GMAIL.COM>
2017-05-25 18:15:06 -0400

    css work on edit and create page, tried start server and made some changes on secrets and carrier

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,10 @@
 CarrierWave.configure do |config|
 config.fog_credentials = {
     :provider                           => 'Google',
-    # :google_storage_access_key_id       => Rails.application.secrets.google_storage_key,
-    # :google_storage_secret_access_key   => Rails.application.secrets.google_storage_secret,
-    :google_storage_access_key_id       => 'GOOGLE_STORAGE_KEY',
-    :google_storage_secret_access_key   => 'GOOGLE_STORAGE_SECRET',
+    :google_storage_access_key_id       => Rails.application.secrets.google_storage_key,
+    :google_storage_secret_access_key   => Rails.application.secrets.google_storage_secret,
+
 
 }
-config.fog_directory = 'juice_bucket'
+config.fog_directory = 'juice_project'
 end
